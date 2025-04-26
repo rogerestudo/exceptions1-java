c621e0f5c18572581893ae841b924a6d37703a4b
Roger William <rogermatos09@hotmail.com>
2025-04-26 16:54:48 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) throws ParseException{
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
+        Reservation reservation = new Reservation();
         Scanner sc = new Scanner(System.in);
         System.out.print("Room number: ");
         int roomNumber = sc.nextInt();
@@ -21,28 +21,22 @@ public class Program {
         if(!checkOut.after(checkIn)){
             System.out.println("Error in reservation: Check-out date must be after check-in date");
         }else{
-            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            reservation = new Reservation(roomNumber, checkIn, checkOut);
             System.out.println("Reservation: " + reservation);
-            System.out.print("Check-in date (dd/MM/yyyy): ");
-            checkIn = sdf.parse(sc.next());
-            System.out.print("Check-out date (mm/MM/yyyy): ");
-            checkOut = sdf.parse(sc.next());
-            Date now = new Date();
-            if(checkIn.before(now) || checkOut.before(now)) {
-                System.out.println("Error in reservation: Reservation dates for update must be future now");
-            }else{
-                if(!checkOut.after(checkIn)){
-                    System.out.println("Error in reservation: Check-out date must be after check-in date");
-                }else{
-                    reservation.updateDate(checkIn, checkOut);
-                    System.out.println("Reservation: " + reservation);
-                }
-                sc.close();
             }
-
+        System.out.print("Check-in date (dd/MM/yyyy): ");
+        checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out date (mm/MM/yyyy): ");
+        checkOut = sdf.parse(sc.next());
+        String error = reservation.updateDate(checkIn, checkOut);
+        if(error != null){
+            System.out.println("Error in reservation: " + error);
+        }else{
+            System.out.println("Reservation: " + reservation);
+        }
         }
 
 
 
-    }
 }
+
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -14,6 +14,7 @@ public class Reservation {
     }
 
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
@@ -35,24 +36,37 @@ public class Reservation {
     public Date getCheckOut() {
         return checkOut;
     }
-    public long duration(){
+
+    public long duration() {
         long diff = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
-    public void updateDate(Date checkIn, Date checkOut){
+
+    public String updateDate(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Error in reservation: Reservation dates for update must be future now";
+        }
+        if (!checkOut.after(checkIn)) {
+                return "Error in reservation: Check-out date must be after check-in date";
+            }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-    }
-    @Override
-    public String toString(){
-        return "Room "
-            +roomNumber
-            +", checkIn: "
-            + sdf.format(checkIn)
-            +", checkOut: "
-            + sdf.format(checkOut)
-            +", "
-            + duration()
-            +" nights";
-    }
-}
+
+        return null;
+        }
+
+            @Override
+            public String toString () {
+                return "Room "
+                        + roomNumber
+                        + ", checkIn: "
+                        + sdf.format(checkIn)
+                        + ", checkOut: "
+                        + sdf.format(checkOut)
+                        + ", "
+                        + duration()
+                        + " nights";
+            }
+        }
+
